84787f54dd6212cf3dbcedf65fff42ab9c1ec1dc
Ahmad Spierij <ahmadspierij@gmail.com>
2025-11-20 17:36:31 -0500

    Adding palyer animation Idle, Jump, Fall, and Run

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,24 @@ public class PlayerController : MonoBehaviour
 {
     // Public variables appear in the Inspector, so you can tweak them without editing code.
     public float moveSpeed = 4f;       // How fast the player moves left/right
-    
+
     //Jump realated variables for the Jump Feature (later)
-    //public float jumpForce = 8f;      // How strong the jump is (vertical speed)
-    //public Transform groundCheck;      // Empty child object placed at the player's feet
-    //public float groundCheckRadius = 0.2f; // Size of the circle used to detect ground
-    //public LayerMask groundLayer;      // Which layer counts as "ground" (set in Inspector)
+    public float jumpForce = 8f;      // How strong the jump is (vertical speed)
+    public Transform groundCheck;      // Empty child object placed at the player's feet
+    public float groundCheckRadius = 0.2f; // Size of the circle used to detect ground
+    public LayerMask groundLayer;      // Which layer counts as "ground" (set in Inspector)
 
     // Private variables are used internally by the script.
     private Rigidbody2D rb;            // Reference to the Rigidbody2D component
-    //private bool isGrounded;           // True if player is standing on ground
+    private bool isGrounded;           // True if player is standing on ground
+
+    private Animator animator;      // Reference to the Animator component
 
     void Start()
     {
         // Grab the Rigidbody2D attached to the Player object once at the start.
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -33,6 +36,7 @@ public class PlayerController : MonoBehaviour
         // --- Ground check ---
         // Create an invisible circle at the GroundCheck position.
         // If this circle overlaps any collider on the "Ground" layer, player is grounded.
+        */
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
         // --- Jump ---
@@ -43,6 +47,35 @@ public class PlayerController : MonoBehaviour
             // Horizontal velocity stays the same.
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-        */
+       
+        //
+        SetAnimation(moveInput);
+    }
+
+
+    private void SetAnimation(float moveInput)
+    {
+        if (isGrounded)
+        {
+            if (moveInput != 0)
+            {
+                animator.Play("Player_Run");
+            }
+            else
+            {
+                animator.Play("Player_Idle");
+            }
+        }
+        else
+        {
+            if(rb.linearVelocity.y > 0)
+            {
+                animator.Play("Player_Jump");
+            }
+            else
+            {
+                animator.Play("Player_Fall");
+            }
+        }
     }
 }
