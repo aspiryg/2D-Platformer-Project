5181447e1c023c343e355644d84d4b03fc3b2a59
Pardeep Kaur <pardeepkaur34@Student.SL.On.Ca>
2025-11-20 19:31:28 -0500

    Adding taking damage feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int health = 100;
     // Public variables appear in the Inspector, so you can tweak them without editing code.
     public float moveSpeed = 4f;       // How fast the player moves left/right
 
     //Jump realated variables for the Jump Feature (later)
-    public float jumpForce = 10f;      // How strong the jump is (vertical speed)
+    public float jumpForce = 8f;      // How strong the jump is (vertical speed)
     public Transform groundCheck;      // Empty child object placed at the player's feet
     public float groundCheckRadius = 0.2f; // Size of the circle used to detect ground
     public LayerMask groundLayer;      // Which layer counts as "ground" (set in Inspector)
@@ -17,6 +19,7 @@ public class PlayerController : MonoBehaviour
 
     private Animator animator;      // Reference to the Animator component
 
+    private SpriteRenderer spriteRenderer;
     public int extraJumpsValue = 1;
     private int extraJumps;
     void Start()
@@ -24,6 +27,7 @@ public class PlayerController : MonoBehaviour
         // Grab the Rigidbody2D attached to the Player object once at the start.
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         extraJumps = extraJumpsValue;
     }
@@ -45,7 +49,7 @@ public class PlayerController : MonoBehaviour
         // Create an invisible circle at the GroundCheck position.
         // If this circle overlaps any collider on the "Ground" layer, player is grounded.
         */
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
         // --- Jump ---
         // If player is grounded AND the Jump button (Spacebar by default) is pressed:
@@ -55,7 +59,7 @@ public class PlayerController : MonoBehaviour
             {
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             }
-            else if (extraJumps > 0)
+            else if (extraJumps >0)
             {
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
                 extraJumps--;
@@ -68,12 +72,6 @@ public class PlayerController : MonoBehaviour
         //
         SetAnimation(moveInput);
     }
-
-    private void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-    }
-
 
 
     private void SetAnimation(float moveInput)
@@ -100,5 +98,31 @@ public class PlayerController : MonoBehaviour
                 animator.Play("Player_Fall");
             }
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Damage")
+        {
+            health -= 25;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            StartCoroutine(BlinkRed());
+
+            if(health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    private IEnumerator BlinkRed()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
+    }
+
+    private void Die()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
     }
 }
